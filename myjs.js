f3debff450bcfa7d510acf75d319b6135f371145
AMRENDRAA <68627259+AMRENDRAA@users.noreply.github.com>
2021-05-28 17:21:19 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,22 @@
+m = 0;
+var y;
+function start() {
+    y = setInterval(run, 1000);
+    function run() {
+        if (m == 1200) {
+            clearInterval(x);
+            m = 0;
+
+        }
+        else {
+            m += 100;
+            var x = document.getElementById("img");
+            x.style.marginLeft = m + 'px';
+        }
+    }
+
+}
+function stop() {
+    clearInterval(y)
+
+}
